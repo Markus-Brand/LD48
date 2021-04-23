68588fb094c64f6f5f4e5b5ab605b17d523bb300
Erik Brendel <erik.brendel@student.hpi.de>
2021-04-23 21:21:17 +0200

    make normals work maybe

--- Unity/Assets/Scripts/PlayerMovement.cs
+++ Unity/Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-	public float MovementSpeed = 1f; // only default value
-	public float RotationSpeed = 1f; // only default value
+	public float movementSpeed = 1f; // only default value
+	public float rotationSpeed = 1f; // only default value
 
 	void Start()
 	{
@@ -28,7 +28,7 @@ public class PlayerMovement : MonoBehaviour
 			direction += Vector2.right;
 		}
 		if (direction.sqrMagnitude > 0.5) {
-			direction *= MovementSpeed * Time.deltaTime;
+			direction *= movementSpeed * Time.deltaTime;
 			gameObject.transform.position += new Vector3(direction.x, direction.y, 0);
 		}
 		float rotation = 0;
@@ -39,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
 			rotation += 1;
 		}
 		if (Math.Abs(rotation) > 0.5) {
-			gameObject.transform.Rotate(Vector3.forward, rotation * Time.deltaTime * RotationSpeed);
+			gameObject.transform.Rotate(Vector3.forward, rotation * Time.deltaTime * rotationSpeed);
 		}
 	}
 }
