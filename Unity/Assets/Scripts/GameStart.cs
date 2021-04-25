15f497802ea5a88da0fd9aeeecbd8e58fa30f261
Erik Brendel <erik.brendel@student.hpi.de>
2021-04-25 09:00:02 +0200

    use #if UNITY_EDITOR for dev scripts

--- Unity/Assets/Scripts/GameStart.cs
+++ Unity/Assets/Scripts/GameStart.cs
@@ -6,9 +6,11 @@ public class GameStart : MonoBehaviour
 {
 	private void Start()
 	{
+#if UNITY_EDITOR
 		FactMethods.CheckCompleteness();
 		FactTopicMethods.CheckCompleteness();
 		FactMethods.GeneratePuml();
+#endif
 		
 		EventManager.getInstance().On<FactStateChangedEvent>(e =>
 			Debug.Log(Notebook.GetNotebookFactText()));
