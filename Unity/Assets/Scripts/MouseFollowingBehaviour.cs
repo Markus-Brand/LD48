6f764e1046a2a9e59bbce732787a3e5e3c7b0035
Markus-PC\Markus <markus.brand@student.hpi.de>
2021-04-25 11:34:27 +0200

    Lantern smoothing

--- Unity/Assets/Scripts/MouseFollowingBehaviour.cs
+++ Unity/Assets/Scripts/MouseFollowingBehaviour.cs
@@ -6,20 +6,31 @@ using UnityEngine;
 public class MouseFollowingBehaviour : MonoBehaviour
 {
     private Camera _camera;
+    public bool smooth;
 
     // Start is called before the first frame update
     void Start()
     {
         _camera = Camera.main;
+        var newPositon = _camera.ScreenToWorldPoint(Input.mousePosition);
+        newPositon.z = 0;
+        transform.position = newPositon;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (_camera != null) {
-            var newPositon = _camera.ScreenToWorldPoint(Input.mousePosition);
-            newPositon.z = 0;
-            transform.position = newPositon;
+            if (smooth) {
+                var targetPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+                targetPosition.z = 0;
+                transform.position += (targetPosition - transform.position) * (Time.deltaTime * 10);
+            } else {
+                var newPositon = _camera.ScreenToWorldPoint(Input.mousePosition);
+                newPositon.z = 0;
+                transform.position = newPositon;
+                
+            }
         }
     }
 }
