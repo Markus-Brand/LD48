d87545494f62118a0d38359f3dc53394432bf828
Erik Brendel <erik.brendel@student.hpi.de>
2021-04-26 18:45:51 +0200

    hide severin after initial dialogue

--- Unity/Assets/Scripts/FactSources/InitialSceneDialogue.cs
+++ Unity/Assets/Scripts/FactSources/InitialSceneDialogue.cs
@@ -2,19 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(DialogueOption))]
-public class SceneDialogue : MonoBehaviour
+public class InitialSceneDialogue : MonoBehaviour
 {
+	public UnityEvent OnStart;
+	
 	private static readonly HashSet<string> _triggeredScenes = new HashSet<string>();
 	
 	public void Start()
 	{
 		var sceneName = SceneManager.GetActiveScene().name;
 		if (_triggeredScenes.Contains(sceneName)) return;
-		
 		_triggeredScenes.Add(sceneName);
+		OnStart?.Invoke();
 		var option = GetComponent<DialogueOption>();
 		Invoker.InvokeUnscaled(() => option.Execute(), 0.1f);
 		// option.Execute();
