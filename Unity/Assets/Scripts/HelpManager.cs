4726f6da6652353364bce064b4b5289d0857ddd0
Markus-PC\Markus <markus.brand@student.hpi.de>
2021-04-26 23:09:17 +0200

    Way to close game and show controls ingame

--- Unity/Assets/Scripts/HelpManager.cs
+++ Unity/Assets/Scripts/HelpManager.cs
@@ -1,12 +1,23 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HelpManager : MonoBehaviour
 {
 	private static DialogueManager Dm => DialogueManager.GetInstance();
-	
+	public DialogueOption controls;
 	private void Update()
 	{
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (!Dm.IsCurrentlyActive) {
+				DialogueManager.ChoiceOption[] choices = {
+					new DialogueManager.ChoiceOption("Controls", () => controls.Execute()),
+					new DialogueManager.ChoiceOption("Exit Game", Application.Quit),
+					new DialogueManager.ChoiceOption("Nothing", () => {})
+				};
+				Dm.ShowChoice(choices);
+			}
+		}
 		if (Input.GetKeyDown(KeyCode.H)) {
 			if (!Dm.IsCurrentlyActive) {
 				var discoverableFacts = FactManager.Instance.AllDiscoverableFacts.Where(f => f.HelpText.Length > 0).ToList();
