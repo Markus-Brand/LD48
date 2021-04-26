5fee1b7bfeba3e784388f8421c59c7ebda4b8277
Fabian Pottbäcker <fabian.pottbaecker@student.hpi.de>
2021-04-26 17:54:30 +0200

    Invisible Queen

---
+++ Unity/Assets/Scripts/Util/FactConditionalDisabler.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using WgEventSystem;
+using WgEventSystem.Events;
+
+public class FactConditionalDisabler : MonoBehaviour
+{
+	public bool Invisible;
+
+	public FactReference VisibleOnce;
+	public FactReference VisibleUntil;
+
+	public bool Present => (VisibleOnce.IsNull || VisibleOnce.Discovered) &&
+	                       (VisibleUntil.IsNull || !VisibleUntil.Discovered);
+
+	private SpriteRenderer _spriteRenderer;
+	private Collider2D _collider;
+	
+	private void Start()
+	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_collider = GetComponent<Collider2D>();
+		UpdateVisibilty();
+		EventManager.getInstance().On<FactStateChangedEvent>(e => UpdateVisibilty());
+	}
+
+	public void UpdateVisibilty()
+	{
+		if (Invisible && _spriteRenderer != null) _spriteRenderer.enabled = Present;
+		if (_collider != null) _collider.enabled = Present;
+	}
+}
