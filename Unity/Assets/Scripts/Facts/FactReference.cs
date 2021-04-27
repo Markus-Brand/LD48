d90a646de311cbfe6da1c347f8fc771ee88a4348
Erik Brendel <erik.brendel@student.hpi.de>
2021-04-27 03:42:58 +0200

    maybe more fixes?

--- Unity/Assets/Scripts/Facts/FactReference.cs
+++ Unity/Assets/Scripts/Facts/FactReference.cs
@@ -6,7 +6,7 @@ public class FactReference
 {
 	public string FactID;
 
-	public bool IsNull => FactID == "";
+	public bool IsNull => string.IsNullOrEmpty(FactID);
 	public FactState State {
 		get => !IsNull ? FactManager.GetFactState(FactID) : FactState.Discovered;
 		set => FactManager.SetFactState(FactID, value);
