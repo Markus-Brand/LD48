2fb6c00481ec84dbe75b14806b578f0bd17b2528
Erik Brendel <erik.brendel@student.hpi.de>
2021-04-27 02:40:34 +0200

    New Fact Popup

--- Unity/Assets/Scripts/EventSystem/Events/FactStateChangedEvent.cs
+++ Unity/Assets/Scripts/EventSystem/Events/FactStateChangedEvent.cs
@@ -2,5 +2,13 @@ namespace WgEventSystem.Events
 {
 	public class FactStateChangedEvent : GameEvent
 	{
+		public readonly string FactId;
+		public readonly FactState NewState;
+
+		public FactStateChangedEvent(string factId, FactState newState)
+		{
+			this.FactId = factId;
+			this.NewState = newState;
+		}
 	}
 }
